e20fc304cc09feff9c4f62ca2f88276cabee0c0d
unknown <rinaldiwaaw91@gmail.com>
2018-08-15 14:47:21 +0700

    relationship created

--- app/Model/Country.php
+++ app/Model/Country.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Region;
 use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
-    //
+    public function Region()
+    {
+    	return $this->hasMany(Region::class);
+    }
 }
--- app/Model/Region.php
+++ app/Model/Region.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Country;
 use Illuminate\Database\Eloquent\Model;
 
 class Region extends Model
 {
-    //
+    public function Country()
+    {
+    	return $this->belongsTo(Country::class);
+    }
 }
